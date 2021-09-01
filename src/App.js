7bf41c93ff763ecab87ee6a4faf9fbe757e94745
CyberVanessa <vanessa.combet@gmail.com>
2021-09-01 13:28:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Garden from "./Garden"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { farm: [] }
+      this.SIZE_GARDEN = 25
+      //garden: Array.from(Array(25).keys())
+  }
+
+  generateGarden = (size) => {
+      let newGarden = []
+      for (let i = 0; i < size; i++) {
+          newGarden.push({ id: i, emoji: ""})
+      }
+      return newGarden
+  }
+
+  addGarden = (size) => {
+      let newFarm = [...this.state.farm]
+      newFarm.push(this.generateGarden(size))
+      this.setState( {farm: newFarm} )
+  }
+
+  componentDidMount() {
+      //this.setState({ garden: this.generateGarden(size_garden)})
+      this.addGarden(this.SIZE_GARDEN)
+      //this.setState( {} )
+  }
+
+  componentDidUpdate() {
+      console.log("farm", this.state.farm)
+  }
+
+  render() {
+    return (
+      <>
+      <h1>F... this Emoji Garden</h1>
+      {this.state.farm.map((elem) => {
+          return (
+              <div className="gdn-container">
+                  <Garden garden={elem} />
+              </div>
+          )
+      })}
+
+      <div className="btn-container">
+          <button onClick={() => this.addGarden(this.SIZE_GARDEN)} id="button-garden" type="submit" >Add a garden</button>
+      </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Garden.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import "./App.css"
+
+class Garden extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { garden: props.garden}
+    }
+
+    componentDidUpdate() {
+        console.log("nb of emojis", this.getNbOfEmojis())
+    }
+
+    getNbOfEmojis = () => {
+        let nbOfEmojis = this.state.garden.filter( (elem) => elem.emoji !== "")
+        return nbOfEmojis.length
+    }
+
+    addPlant = () => {
+        this.growGarden()
+        if (this.getNbOfEmojis() < 25) {
+            let newGarden = [...this.state.garden]
+            newGarden.find(elem => elem.emoji === "").emoji = "🌱";
+            this.setState({ garden: newGarden })
+        }
+    }
+
+    growGarden = () => {
+        let newGarden = [...this.state.garden];
+        newGarden.map((elem) => {
+            if(elem.emoji === "🌱") return elem.emoji = "🌿"
+            if(elem.emoji === "🌿") return elem.emoji = "🌳"
+            else return elem.emoji
+        })
+        this.setState({ garden: newGarden })
+        if (this.getNbOfEmojis() > 0) {
+            setTimeout(() => {
+                this.growGarden()
+            }, 2000);
+        }
+    }
+
+    deleteEmoji = (id) => {
+        let newGarden = [...this.state.garden]
+        newGarden[id].emoji = ''
+        this.setState({ garden: newGarden, count: this.state.count - 1 })
+        //this.setState({ count: this.state.count - 1 })
+    }
+
+    render() {
+        let ligne = this.state.garden.map((cell, index) => {
+            return(
+                <div className="cell" key={cell.id} onClick={() => this.deleteEmoji(cell.id)}>
+                    {cell.emoji}
+                </div>)
+            })
+            return (
+                <>
+                <div className="grid-container">
+                    {ligne}
+                </div>
+                <div className="btn-container">
+                    <button id="button-plant" type="submit" onClick={this.addPlant}>Plant an Emoji</button>
+                </div>
+                </>
+            )
+    }
+}
+
+export default Garden;
